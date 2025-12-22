f1dc50c37afb9af748e0203c2abb6d4a25393673
trassert <trassert86@gmail.com>
2025-12-22 15:11:14 +0300

    Авторелизы, часть 11

--- .github/scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -X POST https://api.modrinth.com/v2/version \
-  -H "Authorization: $MODRINTH_TOKEN" \
-  -F "data=@modrinth.mod.json;type=application/json" \
-  -F "file=@modpack.zip;filename=modpack.mrpack;type=application/x-modrinth-modpack"
